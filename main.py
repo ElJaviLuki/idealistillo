b74a636e8cf93e928779dbf85c33630cb8803a6c
Javier <eljaviluki@gmail.com>
2025-02-16 19:13:02 +0100

    Save different csv's for different sources

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 from api import IdealistaClient, USER, PASSWORD
 from builder import SearchFilterBuilder
+from data_cleaning import clean_data
 from model import *
 from service import IdealistaService
 
@@ -30,7 +31,12 @@ if e:
     builder = (SearchFilterBuilder()
                .set_operation(Operation.RENT)
                .set_property_type(PropertyType.HOMES)
-               .set_price_range(0, 1500)
                .set_shape_from_json(shape))
-    for result in ic.search_with_paginated_results(builder):
-        pass
+    pr = [result for result in ic.search_with_paginated_results(builder)]
+    result = clean_data(pr)
+    result['properties'].to_csv('properties.csv', index=False)
+    result['videos'].to_csv('videos.csv', index=False)
+    result['images'].to_csv('images.csv', index=False)
+    result['virtual3DTours'].to_csv('virtual3DTours.csv', index=False)
+    result['agencies'].to_csv('agencies.csv', index=False)
+    print(pr)
